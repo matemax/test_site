a73ff3e08a32d6301ba21cd2233009a3ec2f3919
Maxim Limonov <matemaks@ya.ru>
2016-10-06 23:39:08 +0300

    refactoring

--- app/views.py
+++ app/views.py
@@ -14,13 +14,22 @@ from flask import stream_with_context, make_response, render_template
 
 from config import LUNA_URL
 
-@app.route('/view/<lessons>')
+
+@app.route('/lessons')
+def lessons():
+    return render_template("lessons_main.html")
+
+@app.route('/lessons/<lessons>')
 def testpdfviewer(lessons):
     lessonspdf = lessons + '.pdf'
     return render_template("view_lessons.html", pdfName = lessonspdf)
 
 
 
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 @app.route('/web')
 def web():
