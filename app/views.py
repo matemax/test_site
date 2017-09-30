a53f63399827b81cc4bd33e31ae29b1fc3bba068
matemax <matemaks@ya.ru>
2017-09-30 10:08:55 +0300

    new year

--- app/views.py
+++ app/views.py
@@ -13,11 +13,28 @@ def testpdfviewer(lessons):
     lessonspdf = lessons + '.pdf'
     return render_template("view_lessons.html", pdfName = lessonspdf)
 
-
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
 
+@app.route('/files_/<filename>')
+def filesHandler(filename):
+    return app.send_static_file('archive/2016-2017/' + filename)
 
+
+@app.route('/archive/<years>/<filename>')
+def archiveFilesHandler(years, filename):
+    return app.send_static_file('archive/' + years + '/' + filename)
+
+@app.route('/archive')
+def archive():
+    return render_template("archive.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
---
+++ make_archive.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+def make_archive(years):
+    res = '<ul class="list-group">'
+    files = [f for f in os.listdir('./app/static/archive/' + years) if  (f.endswith("pdf"))]
+    getTex = lambda x: x[:-3] + 'tex'
+    for f in files:
+        res += '<li class="list-group-item list-group-item-primary">'
+        res += '<a href="/archive/' + years + '/' + f + '">'
+        res += f[3:-4] + '</a>' +'(<a href="/archive/' + years + '/'   + getTex(f)  + '">' + 'tex'+ '</a>)' +  '</li>\n'
+    res += '</ul>'
+
+    with open('./app/templates/archive' + years+ ".html", 'w', encoding="utf-8") as f:
+        f.write(res)
+
+
+def make_archive2(years):
+    res = '<table class="table">\n <tbody>'
+    files = [f for f in os.listdir('./app/static/archive/' + years) if  (f.endswith("pdf"))]
+    countRows  = int((len(files) + 2) / 3)
+    getTex = lambda x: x[:-3] + 'tex'
+
+    for row in range(countRows):
+        res += "\n<tr>"
+        for i in range(3):
+            if row + i * countRows >= len(files):
+                res += "\n<td>\n"
+                res += "\n</td>\n"
+                continue
+            f = files[row + i * countRows]
+            res += "\n<td>\n"
+            res  += '<a href="/archive/' + years + '/' + f + '">'
+            res += f[3:-4] + '</a>' + '(<a href="/archive/' + years + '/' + getTex(
+                f) + '">' + 'tex' + '</a>)' + '\n'
+            res+= "\n</td>\n"
+        res += "\n</tr>"
+    #
+    # for count, f in enumerate(files):
+    #     res += '<li class="list-group-item list-group-item-primary">'
+    #     res += '<a href="/archive/' + years + '/' + f + '">'
+    #     res += f[3:-4] + '</a>' +'(<a href="/archive/' + years + '/'   + getTex(f)  + '">' + 'tex'+ '</a>)' +  '</li>\n'
+    res += ' </tbody>\n</table>'
+
+    with open('./app/templates/archive' + years+ ".html", 'w', encoding="utf-8") as f:
+        f.write(res)
+
+
+make_archive2("2016-2017")
